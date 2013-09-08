7a110129926bd6b483fe3253fec73f5a2a54aa50
Bill Kernan <wkernan@gmail.com>
2013-09-08 10:34:52 -0500

    initial and fb login

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+gem 'jquery-rails'
+gem 'omniauth-facebook', '1.4.0'
+
+
+group :production do
+	gem 'pg'
+end
+
+group :development, :test do
+	gem 'sqlite3'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.2.2.0'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Continder::Application.config.secret_token = 'd824b690057268b908b47125b6d4fdb0bc05f2c8e271f62c9d4363d33fa6669bfbd8a9f2b9d1078b2680e88a0544aed2cbfb5d27aa8ac19c270655e58695f852'
