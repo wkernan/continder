7ad7fd910a3962ba453a9037c3f7ac18859292c9
Bill Kernan <wkernan@gmail.com>
2013-09-28 09:50:54 -0500

    random stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '158506181011684', 'f6a508043410d248b962e2cb424a860c',
-  {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  provider :facebook, '158506181011684', 'f6a508043410d248b962e2cb424a860c'
 end
