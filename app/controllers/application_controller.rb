7c7c6e70821a928445c8de266df5f4f554529409
Bill Kernan <wkernan@gmail.com>
2013-10-13 18:16:52 -0500

    admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,10 @@ private
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
+
+	def admin?
+		request.remote_ip == "127.0.0.1"
+	end
+	helper_method :admin?
 	helper_method :current_user
 end
