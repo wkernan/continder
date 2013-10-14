b40721284b6e7815abae3f8a5a6db6e2a8e8a8b7
Bill Kernan <wkernan@gmail.com>
2013-10-13 22:41:11 -0500

    mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Continder::Application.initialize!
 
-APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
+#needed to get mailer to work
+#APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
