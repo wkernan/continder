405d228fc01772d835463d415f142082307cd173
Bill Kernan <wkernan@gmail.com>
2013-10-13 22:42:27 -0500

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,17 @@ Continder::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { :host => 'continder.com' }
+  
+  #umark all below to get mailer working
+  #config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain", 
-    enable_starttls_auto: true,
-    user_name: APP_CONFIG['username'],
-    password: APP_CONFIG['password']
-  }
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  authentication: "plain", 
+  #  enable_starttls_auto: true,
+  #  user_name: APP_CONFIG['username'],
+  #  password: APP_CONFIG['password']
+  #}
 
 end
