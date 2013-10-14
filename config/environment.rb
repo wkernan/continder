4269ac592e82febadbbf13829175cd879085b414
Bill Kernan <wkernan@gmail.com>
2013-10-13 23:08:35 -0500

    keys

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Continder::Application.initialize!
 
-#needed to get mailer to work
-APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")
