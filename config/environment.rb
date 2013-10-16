3dc0f97145305ac4d8864610fb31b7e0260dcdf5
Bill Kernan <wkernan@gmail.com>
2013-10-15 21:26:17 -0500

    env again

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Continder::Application.initialize!
-	config.action_mailer.delivery_method = :smtp
-
-  	config.action_mailer.smtp_settings = {
-	    address: "smtp.gmail.com",
-	    port: 587,
-	    domain: 'gmail.com',
-	    :authentication => :plain, 
-	    user_name: 'USERNAME',
-	    password: 'PASSWORD',
-	    enable_starttls_auto: true
- 	}
+require 'tlsmail'
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.delivery_method = :smtp
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+	address: "smtp.gmail.com",
+	port: 587,
+	domain: 'gmail.com',
+	:authentication => :plain, 
+	user_name: 'USERNAME',
+	password: 'PASSWORD',
+	enable_starttls_auto: true
+}
