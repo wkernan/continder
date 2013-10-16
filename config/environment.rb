e123ff8b53b415dc3b919931ea1ca0464da4251d
Bill Kernan <wkernan@gmail.com>
2013-10-15 21:11:37 -0500

    env

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Continder::Application.initialize!
+	config.action_mailer.delivery_method = :smtp
 
+  	config.action_mailer.smtp_settings = {
+	    address: "smtp.gmail.com",
+	    port: 587,
+	    domain: 'gmail.com',
+	    :authentication => :plain, 
+	    user_name: 'USERNAME',
+	    password: 'PASSWORD',
+	    enable_starttls_auto: true
+  }
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,15 +68,5 @@ Continder::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   #umark all below to get mailer working
-  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: 'gmail.com',
-    :authentication => :plain, 
-    user_name: 'USERNAME',
-    password: 'PASSWORD',
-    enable_starttls_auto: true
-  }
 end
