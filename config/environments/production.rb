264681a1178c97d0211c81eb875ff37977b2f5e9
Bill Kernan <wkernan@gmail.com>
2013-10-15 21:30:37 -0500

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,20 @@ Continder::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
   #umark all below to get mailer working
-
+  require 'tlsmail'
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    :authentication => :plain, 
+    user_name: 'USERNAME',
+    password: 'PASSWORD',
+    enable_starttls_auto: true
+  }
 end
