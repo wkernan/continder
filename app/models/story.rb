4c15b9328bbe883bc638972076878364b8ef3799
wkernan <wkernan@gmail.com>
2014-05-18 22:33:05 -0500

    changed voting icons

--- app/models/story.rb
+++ app/models/story.rb
@@ -6,4 +6,8 @@ class Story < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   has_reputation :votes, source: :user, aggregated_by: :sum
+
+  def self.latest
+  	Story.order(:updated_at).last
+  end
 end
